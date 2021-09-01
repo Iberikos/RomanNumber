c7253afabc071e57239d9b38c083912b7714cafa
Iberikos <Ruben89pm@gamil.com>
2021-09-01 11:40:46 +0200

    Funcion a romano OK

---
+++ romanos.py
@@ -0,0 +1,37 @@
+simbolos = {
+    'unidades':['','I','II','III','IV','V','VI','VII','VIII','IX'],
+    'decenas': ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'],    
+    'centenas': ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],
+    'millares': ['','M','MM','MMM']
+}
+
+def validar(n):
+    if not isinstance(n, int):
+        raise ValueError("{} debe ser un entero".format(n))
+
+    if n < 0 or n > 3999:
+        raise ValueError("{} debe de estar entre 0 y 3999".format(n))
+    
+def a_romano(n):
+    validar(n)
+    c=str(n)
+    if len(c) >= 1:
+        unidades = int(c[-1])
+        
+    if len(c) >= 2:
+        decenas = int (c[-2])
+
+    if len(c) >= 3:
+        centenas = int(c[-3])
+        
+    if len(c) >= 4:
+        millares = int (c[-4])
+        
+    componetes = (millares, centenas, decenas, unidades)
+    
+    return simbolos['millares'][millares] + simbolos['centenas'][centenas] + simbolos['decenas'][decenas] + simbolos['unidades'][unidades]
+
+
+
+
+
